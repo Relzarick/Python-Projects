b7a2e81cbcb5cfacf495848255680c6ed12ab8ce
Exodus <Relzarick@gmail.com>
2025-03-12 16:56:53 +0800

    finished lesson

--- lessons/Sudoku Solver (Classes&Objects).py
+++ lessons/Sudoku Solver (Classes&Objects).py
@@ -2,6 +2,15 @@ class Board:
     def __init__(self, board):
         self.board = board
 
+    #! <__main__.Board object at 0x0000022637536A50> Error(need google this)
+    def __str__(self):
+        board_str = ''
+        for row in self.board:
+            row_str = [str(i) if i else '*' for i in row]
+            board_str += ' '.join(row_str)
+            board_str += '\n'
+        return board_str
+
     def find_empty_cell(self):
         for row, contents in enumerate(self.board):
             try:
@@ -32,6 +41,39 @@ class Board:
                     return False
         return True
     
+    #* self. so python knows I'm referencing the methods
+    def is_valid(self, empty, num):
+        row, col = empty
+        valid_in_row = self.valid_in_row(row, num)
+        valid_in_col = self.valid_in_col(col, num)
+        valid_in_square = self.valid_in_square(row, col, num)
+        return all([valid_in_row, valid_in_col, valid_in_square])
+    
+    def solver(self):
+        #* Base case is when all cells are filled up
+        if (next_empty := self.find_empty_cell()) is None:
+            return True
+        for guess in range(1, 10):
+            if self.is_valid(next_empty, guess):
+                row, col = next_empty
+                self.board[row][col] = guess
+                #* recursively call on itself till all solved
+                if self.solver():
+                    return True
+                self.board[row][col] = 0
+        #* return if guesses does not lead to a solution
+        return False
+    
+
+def solve_sudoku(board):
+    gameboard = Board(board)
+    print(f'Puzzle to solve:\n{gameboard}')
+    if gameboard.solver():
+        print(f'Solved puzzle:\n{gameboard}')
+    else:
+        print('The provided puzzle is unsolvable.')
+    return gameboard
+    
 puzzle = [
   [0, 0, 2, 0, 0, 8, 0, 0, 0],
   [0, 0, 0, 0, 0, 3, 7, 6, 2],
@@ -44,4 +86,4 @@ puzzle = [
   [1, 7, 0, 0, 0, 6, 0, 0, 5]
 ]
 
-gameboard = Board(puzzle)
+solve_sudoku(puzzle)
