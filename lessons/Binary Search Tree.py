95fc15c680202968542c9bca7a007fdc494f1abb
Exodus <Relzarick@gmail.com>
2025-03-13 16:04:40 +0800

    finished lesson added comments

--- lessons/Binary Search Tree.py
+++ lessons/Binary Search Tree.py
@@ -2,22 +2,22 @@ class TreeNode:
     #* init is used to define vars for other methods to access within the class
     def __init__(self, key):
         self.key = key
+        self.left = None
+        self.right = None
 
     def __str__(self):
         return str(self.key)
     
 class BinarySearchTree:
-
     def __init__(self):
         self.root = None
 
-    #? helper method
+    #! helper methods
     def _insert(self, node, key):
         if node is None:
             return TreeNode(key)
         
-        #* Values smaller than node.key are placed in the left subtree
-        #* Values greater than node.key are placed in the right subtree
+        #* Smaller values goes to the left subtree, larger values to the right.
         #* node.key == current key||Key == child value
         if key < node.key:
             #* using := will overide because its not a treenode????
@@ -42,23 +42,50 @@ class BinarySearchTree:
             node.left = self._delete(node.left, key)
         elif key > node.key:
             node.right = self._delete(node.right, key) 
-        #* else if there are 2 children
+        #* Else block is if there are 2 children
         else:
             #! no idea what is going on here
             if node.left is None:
                 return node.right 
             elif node.right is None:
                 return node.left
+            
+            node.key = self._min_value(node.right) 
+            node.right = self._delete(node.right, node.key)
+
+        return node  
+      
+
+    def _min_value(self, node):
+        while node.left is not None:
+            node = node.left
+        #* Replaces the node after it has been deleted
+        return node.key
 
 
+    #* result = list keys are appended to sorted
+    def _inorder_traversal(self, node, result):
+        if node:
+            self._inorder_traversal(node.left, result)
+            result.append(node.key)
+            self._inorder_traversal(node.right, result)
 
-    #? user facing method
+
+    #! user facing methods
     def insert(self, key):
-        #* Encapsulation: ensures that the user interacts with a simple interface
+        #? Encapsulation: ensures that the user interacts with a simple interface
         self.root = self._insert(self.root, key)
     
     def search(self, key):
         return self._search(self.root, key)
+    
+    def delete(self, key):
+        self.root = self._delete(self.root, key) 
+
+    def inorder_traversal(self):
+        result = []
+        self._inorder_traversal(self.root, result)
+        return result
     
 
 nodes = [50, 30, 20, 40, 70, 60, 80]
@@ -67,3 +94,13 @@ bst = BinarySearchTree()
 
 for node in nodes:
     bst.insert(node)
+
+print('Search for 80:', bst.search(80))
+
+print("Inorder traversal:", bst.inorder_traversal())
+
+bst.delete(40)
+
+print("Search for 40:", bst.search(40))
+
+print('Inorder traversal after deleting 40:', bst.inorder_traversal())
