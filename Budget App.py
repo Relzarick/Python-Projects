5ab74278152532f92e15c2792db3459f7ff3ddef
Exodus <Relzarick@gmail.com>
2025-03-17 20:56:29 +0800

    finished core functions

--- Budget App.py
+++ Budget App.py
@@ -1,4 +1,5 @@
 from itertools import zip_longest
+import math
 
 class Category:
     def __init__(self, category):
@@ -9,6 +10,7 @@ class Category:
         self.withdrawals = 0
 
     #! Use dictionaries for structured data
+    #TODO some of the amount is not 2 decimal places (maybe change it to int and decimal magic first???)
     def __str__(self):
         ledger_str = f'{self.category.center(30, "*")}\n'
 
@@ -25,10 +27,8 @@ class Category:
         if not self.initial and description.lower() == 'deposit':
             self.deposit(amt, "Initial deposit")
             self.initial = True
-            self.total += amt
         elif transfer:
             self.deposit(amt, f"Transfer from {description}")
-            self.total += amt
         else:
             self.ledger.append({'amount': amt, 'description': description})
             self.total += amt
@@ -68,50 +68,69 @@ class Category:
 
 
 food = Category('Food')
-foods = Category('Foods')
-niers = Category('niers')
+foods = Category('Clothing')
+auto = Category('auto')
 
-food.deposit(10000, 'deposit')
-food.transfer(1000, niers)
-food.withdraw(4000, '123456')
-niers.withdraw(1, 'niy')
 
-# food.get_balance()
+
+auto.deposit(1000, 'deposiT') 
+
+#TODO something wrong with how the percentages are calc?????? 🗿
+
+food.deposit(900, 'deposit')
+food.withdraw(405.67, 'milk, cereal, eggs, bacon, bread')
+food.transfer(1000, auto)
+
+
 
 print(food)
-print(niers)
+print(auto)
 
 
-cat_list = [food, niers, foods]
+cat_list = [food, auto, foods]
 
 
 def create_spend_chart(categories):
     output_str = ''
     output_str += 'Percentage spent by category\n'
 
-    category_names = [name.category for name in cat_list]
-    amount = [val.withdrawals for val in cat_list]
+    category_names = [name.category for name in categories]
+    withdrawal = [val.withdrawals for val in categories]
+    total = [val.total for val in categories]
 
-    #? :>2 is the f-str alignment(align right)
-    percent = ' '.join([f'{str(num):>2}|\n' for num in range(100, -1, -10)])
-    output_str += percent
+    percentages = [math.floor((w / t) * 100 / 10) * 10 if t else 0 for w, t in zip(withdrawal, total)]
+    # print(percentages)
+
+    #* Starting from 100 to 0 in 10s
+    for level in range(100, -1, -10):
+        #* :>3 is to right align it (no need for \n because its looping)
+        line = f'{level:>3}|'
+
+        for per in percentages:
+            #* [20, 100, 0] if matches add 0 
+            if per >= level:
+                line += ' o '
+            else:
+                line += '   '
+
+        output_str += f'{line}\n'
     
     #* Each name gets 3 '-' + 1 for spacing
     dashes = '-' * (len(category_names) * 3 + 1)
     output_str += f"    {dashes}\n"
     
-    
-    #* Iterates the longest, fills shorter with ''
-    #* * operator unpacks the names
+    #* Iterates the longest, fills shorter with ''||* operator unpacks the names
     for row in zip_longest(*category_names, fillvalue=" "):
         #* This is to indent the names
         output_str += "     "
         output_str += f"{'  '.join(row)}\n"
-        
     
+    output_str.rstrip()
     print(output_str)
 
-# create_spend_chart(cat_list)
+create_spend_chart(cat_list)
 
-#? (withdrawals/ Total Amount) * 100 (remember to round down)
-#* then represent with 0 and append to the chart
+
+#! legacy code
+#? :>2 is the f-str alignment(align right)
+#? percent = ' '.join([f'{str(num):>2}|\n' for num in range(100, -1, -10)])
