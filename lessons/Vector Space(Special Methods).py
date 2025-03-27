359c86c24c1ab8f6f77dcf39f9b304918850af32
Exodus <junfeng.nyo@gmail.com>
2025-03-27 11:23:34 +0800

    finished lesson

--- lessons/Vector Space(Special Methods).py
+++ lessons/Vector Space(Special Methods).py
@@ -34,16 +34,49 @@ class R2Vector:
             args = [getattr(self, i) * getattr(other, i) for i in vars(self)]
             return sum(args)            
         return NotImplemented
+
     def __eq__(self, other):
         if type(self) != type(other):
             return NotImplemented
+        return all(getattr(self, i) == getattr(other, i) for i in vars(self))
+        
+    def __ne__(self, other):
+        return not self == other
+
+    def __lt__(self, other):
+        if type(self) != type(other):
+            return NotImplemented
+        return self.norm() < other.norm()
+
+    def __gt__(self, other):
+        if type(self) != type(other):
+            return NotImplemented
+        return self.norm() > other.norm()
+
+    def __le__(self, other):
+        return not self > other
+
+    def __ge__(self, other):
+        return not self < other
+
 class R3Vector(R2Vector):
     def __init__(self, *, x, y, z):
         super().__init__(x=x, y=y)
         self.z = z
+        
+    def cross(self, other):
+        if type(self) != type(other):
+            return NotImplemented
+        kwargs = {
+            'x': self.y * other.z - self.z * other.y,
+            'y': self.z * other.x - self.x * other.z,
+            'z': self.x * other.y - self.y * other.x
+        }
+        
+        return self.__class__(**kwargs)
 
-v1 = R2Vector(x=2, y=3)
-v2 = R2Vector(x=0.5, y=1.25)
+v1 = R3Vector(x=2, y=3, z=1)
+v2 = R3Vector(x=0.5, y=1.25, z=2)
 print(f'v1 = {v1}')
 print(f'v2 = {v2}')
 v3 = v1 + v2
@@ -52,6 +85,9 @@ v4 = v1 - v2
 print(f'v1 - v2 = {v4}')
 v5 = v1 * v2
 print(f'v1 * v2 = {v5}')
+v6 = v1.cross(v2)
+print(f'v1 x v2 = {v6}')
+
 
 # * __str__ returns human readable strings
 
@@ -68,6 +104,8 @@ print(f'v1 * v2 = {v5}')
 # * getattr() used for looking up able to return a default value
 #   called explicitly for existing or missing attributes(allows default value)
 
+
+#  t Called under the hood
 # * __add__
 
 # * __sub__
@@ -75,3 +113,13 @@ print(f'v1 * v2 = {v5}')
 # * __mul__
 
 # * __eq__
+
+# * __ne__
+
+# * __lt__
+
+# * __gt__
+
+# * __le__
+
+# * __ge__
