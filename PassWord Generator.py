6e2ed303a80f56c0aa87f26edeb7b55f5e7de703
Exodus <junfeng.nyo@gmail.com>
2025-03-30 19:32:34 +0800

    update to password generation left with custom config method

--- PassWord Generator.py
+++ PassWord Generator.py
@@ -2,10 +2,27 @@ from secrets import choice
 from string import ascii_letters, ascii_uppercase, digits, punctuation
 from time import sleep
 import re
+import os
 
 class PasswordGenerator:
+  def __init__(self):
+    self.random_char = ascii_letters + digits + punctuation
+    self.user_length = None
+
+    self.terminal_width = os.get_terminal_size().columns
+
+    self.num_re = r'\d'
+    self.letter_re = r'[A-Z]'
+    self.ambi_re = r'[o0i1]'
+    
+    self.options = ['yes', 'no', 'y', 'n']
+    self.ambiguous = ['o', '0', 'i', '1']
+
+    self.user_yes = ['yes', 'y']
+    self.user_no = ['no', 'n']
+
   def prompt(self):
-    print('\nWelcome to the Secure Password Generator!\n')
+    print(f"\n{'Welcome to the Secure Password Generator!'.center(self.terminal_width)}\n")
     
     while True:
       sleep(.4)
@@ -29,94 +46,138 @@ class PasswordGenerator:
       else:
           sleep(.5)
           print("\nThat was an invalid option.\n")
-          
-      
+  
   def _generator_express(self):
     str_length = choice(range(10, 17))
-    random_chara = ascii_letters + digits + punctuation
-
-    num_re = r'\d'
-    letter_re = r'[A-Z]'
 
     while True:
-      random_str = ''.join(choice(random_chara) for _ in range(str_length))
+      random_str = ''.join(choice(self.random_char) for _ in range(str_length))
       
       # * Appends missing characters
-      if not re.search(num_re, random_str):
+      if not re.search(self.num_re, random_str):
         random_str += ''.join(choice(digits) for _ in range(4))
-      if not re.search(letter_re, random_str):
+      if not re.search(self.letter_re, random_str):
         random_str += ''.join(choice(ascii_uppercase) for _ in range(4))
 
       random_str = ''.join(choice(random_str) for _ in range(str_length))
 
-      has_num = re.search(num_re, random_str)
-      has_cap = re.search(letter_re, random_str)
+      has_num = re.search(self.num_re, random_str)
+      has_cap = re.search(self.letter_re, random_str)
 
       if has_num and has_cap:
-        break
-
-    return random_str
+        return random_str
 
   def _generator_custom(self, length, ambi, adv):
-    print(length)
-    print(ambi)
-    print(adv)
+    random_str = ''.join(choice(self.random_char) for _ in range(length))
+    user_config, (upper_case, lower_case, digits_num, special_char) = adv
+    
+    ambi_config = False
+    adv_config = False
+    
+    if ambi in self.options and ambi in self.user_yes:
+      ambi_config = True
+      random_str = ''.join(choice(self.random_char) if i in self.ambiguous else i for i in random_str)
+      
+      
+    if user_config in self.options and user_config in self.user_yes:
+      adv_config = True
+      while True:
+        # t this will keep looping until all the requirements are met before breaking
+        pass
+      
+
+    has_num = re.search(self.num_re, random_str)
+    has_cap = re.search(self.letter_re, random_str)
+    has_ambi = re.search(self.ambi_re, random_str)
+    
+
+    # t do the checks then return value if configs are active
+    if ambi_config or adv_config:
+      # t have to add a final validation to check all requiremnet are met first
+      # ? exclude = ['o', '0', 'i', '1'] try this for validation?
+
+      if has_num and has_cap:
+        return random_str
+    
 
   def _prompt_custom(self):
-    user_length = self._prompt_length()
+    self.user_length = self._prompt_length()
     sleep(.3)
     ambi = self._prompt_ambi()
     sleep(.3)
-    adv_settings = self._prompt_comp()
-        
-    print(f"Here is your secure password: {self._generator_custom(user_length, ambi, adv_settings)}\n")
+    user_config = self._prompt_composition()
+
+    # ('y', (1, 1, 1, 1))
+
+    print(f"Here is your secure password: {self._generator_custom(self.user_length, ambi, user_config)}\n")
 
   def _prompt_length(self):
     while True:
       try:
-        user_length = int(input('\nEnter desired password length (8-25): '))
-        if user_length in range(8, 26):
+        user_length = int(input('\nEnter desired password length (10-25): '))
+        if user_length in range(10, 26):
           return user_length
         else:
           sleep(.3)
-          print("\nInvalid input. Please enter a number between 8 and 25.")
+          print("\nInvalid input. Please enter a number between 10 and 25.")
       except ValueError:
         sleep(.3)
         print("\nInvalid input. Please enter a valid number.")
 
 
   def _prompt_ambi(self):
-    print("\nAmbiguous characters are sometimes hard to distinguish, such as: (e.g., “O”, “0”, “l”, “1”)\n")
+    print("\nAmbiguous characters are sometimes hard to distinguish, such as: (e.g. “O”, “0”, “l”, “1”)\n")
     while True:
       query = input('\nWould you like to exclude these ambiguous characters? (Y or N): ').strip().lower()
-      if query in ['yes', 'no', 'y', 'n']:
+
+      if query in self.options:
         return query
       else:
         sleep(.3)
         print('\nInvalid input. Please enter Yes or No.')
   
 
-  def _prompt_comp(self):
-    print("\nBy default, the password will include the following:")
+  def _prompt_composition(self):
+    print("\nBy default, the password will include at least the following:")
     print("1. One uppercase letter")
     print("2. One lowercase letter")
     print("3. One digit")
     print("4. One special character\n")
     while True:
-      adv_settings = input("\nWould you like to also customize these settings? (Y or N): ").strip().lower()
-      if adv_settings in ['yes', 'no', 'y', 'n']:
-        return adv_settings
+      
+      user_config = input("\nWould you like to also customize these settings? (Y or N): ").strip().lower()
+
+      if user_config in self.options:
+        if user_config in self.user_yes:
+          return user_config, self._adv_config()
+        return user_config, (1, 1, 1, 1)
       else:
         sleep(.3)
         print('\nInvalid input. Please enter Yes or No.')
   
 
+  def _adv_config(self):
+    while True:
+      upper_case = int(input("\nEnter the number of uppercase letters to include: "))
+      lower_case = int(input("Enter the number of lowercase letters to include: "))
+      digits = int(input("Enter the number of digits to include: "))
+      special_char = int(input("Enter the number of special characters to include: "))
+
+      total_char = upper_case + lower_case + digits + special_char
+
+      if total_char < self.user_length:
+        return upper_case, lower_case, digits, special_char
+      else:
+        print(f'\nYou have exceeded your desired password length.\n')
+  
 
 
 if __name__ == '__main__':
   main = PasswordGenerator()
   # main.prompt()
-  # main._generator_custom(13, 'yes', 'n')
+  main._prompt_custom()
+  
+
  
 
 # ! Dual mode 
